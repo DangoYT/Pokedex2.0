d7150528e29dac6c8c9e00c0ec29149a8701129f
PabloLerida <146394698+PabloLerida@users.noreply.github.com>
2023-12-11 19:37:06 -0300

    handleOnClick de la cruz de búsqueda y más ajustes de estilos en búsqueda

--- src/Components/Search/Search.jsx
+++ src/Components/Search/Search.jsx
@@ -1,38 +1,56 @@
-import React, { useState } from 'react';
-import style from './search.module.css'
+import React, { useState } from "react";
+import style from "./search.module.css";
 export default function Search({ listaPokemons }) {
-    // Estado local para el término de búsqueda
-    const [searchTerm, setSearchTerm] = useState('');
+  // Estado local para el término de búsqueda
+  const [searchTerm, setSearchTerm] = useState("");
 
-    // Función para manejar cambios en el campo de búsqueda
-    const handleInputChange = (event) => {
-        setSearchTerm(event.target.value);
-    };
+  // Función para manejar cambios en el campo de búsqueda
+  const handleInputChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
 
-    // Filtrar la lista de Pokémon según el término de búsqueda
-    const filteredPokemons = listaPokemons.filter((pokemon) =>
-        pokemon.name.toLowerCase().includes(searchTerm.toLowerCase())
-    );
-    console.log("este es el resultado de los pokemons filtrados", filteredPokemons);
-    return (
-        <div>
-            <input
-                className={style.pokesearch}
-                type="text"
-                value={searchTerm}
-                onChange={handleInputChange}
-                placeholder="Buscar por nombre o ID..."
-            />
+  // Filtrar la lista de Pokémon según el término de búsqueda
+  const filteredPokemons = listaPokemons.filter((pokemon) =>
+    pokemon.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
-            {/* Mostrar resultados filtrados */}
-            <div>
-                <h2>Resultados de la búsqueda:</h2>
-                <ul>
-                    {filteredPokemons.map((pokemon) => (
-                        <li key={pokemon.id}>{pokemon.name}</li>
-                    ))}
-                </ul>
-            </div>
-        </div>
-    );
+  const handleClearClick = () => {
+    setSearchTerm("");
+  };
+
+  console.log(
+    "este es el resultado de los pokemons filtrados",
+    filteredPokemons
+  );
+  return (
+    <div>
+      <div className={style.pokesearch}>
+        <img className={style.pokelupa} src="images_figma/search.svg" alt="" />
+        <input
+          className={style.pokesearchbar}
+          type="text"
+          value={searchTerm}
+          onChange={handleInputChange}
+          placeholder="Buscar por nombre o ID..."
+        />
+        {searchTerm && (
+          <img
+            className={style.pokeclose}
+            src="images_figma/close.svg"
+            alt=""
+            onClick={handleClearClick}
+          />
+        )}
+      </div>
+      {/* Mostrar resultados filtrados */}
+      <div>
+        <h2>Resultados de la búsqueda:</h2>
+        <ul>
+          {filteredPokemons.map((pokemon) => (
+            <li key={pokemon.id}>{pokemon.name}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
