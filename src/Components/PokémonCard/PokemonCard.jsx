2c48d88b7841a9fc6f5da51ff32fd68ed3889bf2
Hector Daniel Gonzalez Laport <danigl_1891@hotmail.com>
2023-12-13 19:58:01 -0300

    Ultimos cambios

--- src/Components/PokémonCard/PokemonCard.jsx
+++ src/Components/PokémonCard/PokemonCard.jsx
@@ -5,32 +5,35 @@ export default function PokemonCard() {
     const location = useLocation();
     const pokemon = location.state;
     console.log(pokemon);
-  return (
-    <div className={style[pokemon.types[0].type.name]}>
-      <p className="pokemon__name">{pokemon.name}</p>
+    return (
+        <div className={`${style[pokemon.types[0].type.name]} `}>
+
+            <p className="pokemon__name">{pokemon.name}</p>
             <span className="pokemon__id">{pokemon.id}</span>
             <img className="pokemon__image" src={pokemon.sprites.other["official-artwork"].front_default} alt="" />
+            <div className={style.contenedor}>
+                <ul className="pokemon__types">
+                    {pokemon.types.map((type, index) => (
+                        <li key={index} className={`${style[pokemon.types[0].type.name]} ${style.contenedor}`}>{type.type.name}</li>
+                    ))}
+                </ul>
 
-            <ul className="pokemon__types">
-                {pokemon.types.map((type, index) => (
-                    <li key={index} className="pokemon__type">{type.type.name}</li>
+                <p className="pokemon__height">{pokemon.height}</p>
+                <p className="pokemon__weight">{pokemon.weight}</p>
+
+                <ul className="pokemon__abilities">
+                    {pokemon.abilities.map((ability, index) => (
+                        <li key={index} className="pokemon__ability">
+                            {ability.ability.name}
+                        </li>
+                    ))}
+                </ul>
+
+                {pokemon.stats.map((stat, index) => (
+                    <p className="pokemon__stats" key={index}>{stat.base_stat}</p>
                 ))}
-            </ul>
+            </div>
 
-            <p className="pokemon__height">{pokemon.height}</p>
-            <p className="pokemon__weight">{pokemon.weight}</p>
-
-            <ul className="pokemon__abilities">
-                {pokemon.abilities.map((ability, index) => (
-                    <li key={index} className="pokemon__ability">
-                        {ability.ability.name}
-                    </li>
-                ))}
-            </ul>
-
-            {pokemon.stats.map((stat, index) => (
-                <p className="pokemon__stats" key={index}>{stat.base_stat}</p>
-            ) )}
-    </div>
-  )
+        </div>
+    )
 }
