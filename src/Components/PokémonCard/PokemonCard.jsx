8b11bb807f86cdf9e5bca9d34168118510b51cea
PabloLerida <146394698+PabloLerida@users.noreply.github.com>
2023-12-15 20:36:26 -0300

    armado de estilos de pokemon card

--- src/Components/PokémonCard/PokemonCard.jsx
+++ src/Components/PokémonCard/PokemonCard.jsx
@@ -41,7 +41,6 @@ export default function PokemonCard() {
     setCurrentPokemonIndex(0);
     setTypeUno(style[selectedPokemon.types[0].type.name]);
     /* setTypeDos(selectedPokemon.types["1"].type.name) */
-
   }, []);
   const handleSlideChange = (swiper) => {
     setCurrentPokemonIndex(swiper.activeIndex);
@@ -49,61 +48,70 @@ export default function PokemonCard() {
   console.log(selectedPokemon);
   const pokemonArray = [selectedPokemon];
 
-  const [typeUno, setTypeUno] = useState("")
+  const [typeUno, setTypeUno] = useState("");
   /* const [typeDos, setTypeDos] = useState("") */
-
 
   return (
     <Swiper onSlideChange={handleSlideChange}>
       {pokemonArray.map((pokemon) => (
         <SwiperSlide key={pokemon.id}>
-          <div className={typeUno}>
-            <p className="pokemon__name">{pokemon.name}</p>
-            <span className="pokemon__id">{pokemon.id}</span>
+          <div className={`${typeUno} ${style.pokemon_card}`}>
+            <div className={style.pokemon__header}>
+              <div className={style.pokemon__name__container}>
+                <img
+                  className={style.pokemon__back}
+                  src="images_figma/arrow_back.svg"
+                  alt=""
+                />
+                <p id={style.pokemon__name} className={style.pokemon__name}>
+                  {pokemon.name}
+                </p>
+              </div>
+              <div className={style.pokemon__id__container}>
+                <span className={style.pokemon__id}>#00{pokemon.id}</span>
+              </div>
+            </div>
             <img
-              className="pokemon__image"
+              className={style.pokemon__img}
               src={pokemon.sprites.other["official-artwork"].front_default}
               alt=""
             />
             <img src={pokeGift} alt="" />
             <div className={style.contenedor}>
               <div>
-                {pokemon.types.map((type) => (<span className={style[type.type.name]}>{type.type.name}</span>))}
+                {pokemon.types.map((type) => (
+                  <span className={style[type.type.name]}>
+                    {type.type.name}
+                  </span>
+                ))}
               </div>
               <p className={style.pokeabout}>About</p>
-              <div>
-                <div>
-                  <p className={style.pokemon__height}>{pokemon.height}</p>
+              <div id={style.pokemon__stats__container} className={style.pokemon__stats}>
+                <div className={style.pokemon__height}>
+                  <img
+                    className={style.heightimg}
+                    src="images_figma/straighten.svg"
+                    alt=""
+                  />
+                  <p>Height</p>
+                  <p>{pokemon.height}</p>
                 </div>
-                <div>
+                <div className={style.pokemon__weight}>
                   {" "}
-                  <p className={style.pokemon__weight}>{pokemon.weight}</p>
+                  <img
+                    className={style.weightimg}
+                    src="images_figma/weight.svg"
+                    alt=""
+                  />
+                  <p>Weight</p>
+                  <p>{pokemon.weight}</p>
                 </div>
-
-                {/* <div>
+                <div className="pokemon__abilities">
                   {pokemon.abilities.map((ability, index) => (
                     <span key={index} className="pokemon__ability">
-                      {ability.ability.name[0]}
+                      {ability.ability.name}
                     </span>
                   ))}
-                </div>
-
-                <div>
-                  {pokemon.abilities.map((ability, index) => (
-                    <span key={index} className="pokemon__ability">
-                      {ability.ability.name[1]}
-                    </span>
-                  ))}
-                </div> */}
-
-                <div>
-                  <ul className="pokemon__abilities">
-                    {pokemon.abilities.map((ability, index) => (
-                      <span key={index} className="pokemon__ability">
-                        {ability.ability.name}
-                      </span>
-                    ))}
-                  </ul>
                 </div>
               </div>
 
@@ -113,24 +121,16 @@ export default function PokemonCard() {
 
               <div className={style.pokestatscontainer}>
                 <div className={style.pokestats}>
-                  <p className={typeUno}>
-                    HP
-                  </p>
-                  <p className={`${style[pokemon.types[0].type.name]} `}>
-                    ATK
-                  </p>
-                  <p className={`${style[pokemon.types[0].type.name]} `}>
-                    DEF
-                  </p>
+                  <p className={typeUno}>HP</p>
+                  <p className={`${style[pokemon.types[0].type.name]} `}>ATK</p>
+                  <p className={`${style[pokemon.types[0].type.name]} `}>DEF</p>
                   <p className={`${style[pokemon.types[0].type.name]} `}>
                     SATK
                   </p>
                   <p className={`${style[pokemon.types[0].type.name]} `}>
                     SDEF
                   </p>
-                  <p className={`${style[pokemon.types[0].type.name]} `}>
-                    SPD
-                  </p>
+                  <p className={`${style[pokemon.types[0].type.name]} `}>SPD</p>
                 </div>
                 <div className={style.pokestatsvalues}>
                   {pokemon.stats.map((stat, index) => (
